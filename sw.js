f7e96b900c125ac25314243d3aa4bf244808f768
guaitoligabriele7-source <guaitoligabriele7@gmail.com>
2026-06-28 19:18:25 +0200

    Add files via upload
    FORGE v8.1 update

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE='forge-v7';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='forge-v8';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
