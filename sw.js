61b4cec56a3ea5b48ba46cf7872253468271cf8d
guaitoligabriele7-source <guaitoligabriele7@gmail.com>
2026-07-15 01:46:25 +0200

    Add files via upload
    V_9_5_2_Engine

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='forge-v9-5-1-engine-fix';
+const CACHE='forge-v9-5-2-audio-academy';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
