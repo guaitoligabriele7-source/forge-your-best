dc709f4e52cc15fdec8e2531c7ff0d3d70a47bcf
guaitoligabriele7-source <guaitoligabriele7@gmail.com>
2026-07-26 18:30:33 +0200

    Add files via upload
    V9_7_2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='forge-v9-7-1-biomechanics';
+const CACHE='forge-v9-7-2-academy-stable';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
